f1f46f90d827a98a920cd23f5e21e8dadba4c35c
beo_clowar <121174604+0xbugbag@users.noreply.github.com>
2024-12-05 00:32:44 +0700

    Update JsonFormats.scala

--- src/main/scala/com/example/JsonFormats.scala
+++ src/main/scala/com/example/JsonFormats.scala
@@ -1,18 +1,14 @@
 package com.example
 
 import com.example.UserRegistry.ActionPerformed
-
-//#json-formats
-import spray.json.RootJsonFormat
 import spray.json.DefaultJsonProtocol
 
-object JsonFormats  {
+object JsonFormats {
   // import the default encoders for primitive types (Int, String, Lists etc)
   import DefaultJsonProtocol._
 
-  implicit val userJsonFormat: RootJsonFormat[User] = jsonFormat3(User.apply)
-  implicit val usersJsonFormat: RootJsonFormat[Users] = jsonFormat1(Users.apply)
-
-  implicit val actionPerformedJsonFormat: RootJsonFormat[ActionPerformed]  = jsonFormat1(ActionPerformed.apply)
+  implicit val userJsonFormat = jsonFormat3(User.apply)
+  implicit val usersJsonFormat = jsonFormat1(Users.apply)
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed.apply)
+  implicit val healthResponseFormat = jsonFormat2(HealthResponse.apply)
 }
-//#json-formats
