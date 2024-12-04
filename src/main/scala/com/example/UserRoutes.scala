e7c1611c1cd94be3ee21953341f20119c5c0e630
beo_clowar <121174604+0xbugbag@users.noreply.github.com>
2024-12-05 00:18:56 +0700

    Update UserRoutes.scala

--- src/main/scala/com/example/UserRoutes.scala
+++ src/main/scala/com/example/UserRoutes.scala
@@ -1,28 +1,21 @@
 package com.example
 
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}  // Add these imports
 import akka.http.scaladsl.server.Route
-
 import scala.concurrent.Future
 import com.example.UserRegistry._
 import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
-import spray.json.DefaultJsonProtocol._
 
-//#import-json-formats
-//#user-routes-class
 class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val system: ActorSystem[_]) extends CorsHandler {
-
-  //#user-routes-class
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import JsonFormats._
-  //#import-json-formats
-
-  // If ask takes more time than this to complete the request is failed
-  private implicit val timeout: Timeout = Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
+  
+  private implicit val timeout: Timeout = 
+    Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
 
   def getUsers(): Future[Users] =
     userRegistry.ask(GetUsers.apply)
@@ -33,16 +26,13 @@ class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val syst
   def deleteUser(name: String): Future[ActionPerformed] =
     userRegistry.ask(DeleteUser(name, _))
 
-  //#all-routes
-  //#users-get-post
-  //#users-get-delete
-  val userRoutes: Route = corsHandler {  // Add corsHandler here
-      concat(
+  val userRoutes: Route = corsHandler {
+    concat(
       // Root path
       pathEndOrSingleSlash {
         get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
-            """
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, 
+            s"""
             |<!DOCTYPE html>
             |<html>
             |<head>
@@ -66,52 +56,43 @@ class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val syst
             """.stripMargin))
         }
       },
-      // Add this health check route
+      // Health check
       path("health") {
         get {
           complete(StatusCodes.OK -> Map("status" -> "UP", "timestamp" -> java.time.Instant.now.toString))
         }
       },
+      // Users routes
       pathPrefix("users") {
-      concat(
-        //#users-get-delete
-        pathEnd {
-          concat(
-            get {
-              complete(getUsers())
-            },
-            post {
-              entity(as[User]) { user =>
-                onSuccess(createUser(user)) { performed =>
-                  complete((StatusCodes.Created, performed))
+        concat(
+          pathEnd {
+            concat(
+              get {
+                complete(getUsers())
+              },
+              post {
+                entity(as[User]) { user =>
+                  onSuccess(createUser(user)) { performed =>
+                    complete((StatusCodes.Created, performed))
+                  }
                 }
-              }
-            })
-        },
-        //#users-get-delete
-        //#users-get-post
-        path(Segment) { name =>
-          concat(
-            get {
-              //#retrieve-user-info
-              rejectEmptyResponse {
-                onSuccess(getUser(name)) { response =>
-                  complete(response.maybeUser)
+              })
+          },
+          path(Segment) { name =>
+            concat(
+              get {
+                rejectEmptyResponse {
+                  onSuccess(getUser(name)) { response =>
+                    complete(response.maybeUser)
+                  }
                 }
-              }
-              //#retrieve-user-info
-            },
-            delete {
-              //#users-delete-logic
-              onSuccess(deleteUser(name)) { performed =>
-                complete((StatusCodes.OK, performed))
-              }
-              //#users-delete-logic
-            })
-        })
-      //#users-get-delete
-    }
-  //#all-routes
-  )
+              },
+              delete {
+                onSuccess(deleteUser(name)) { performed =>
+                  complete((StatusCodes.OK, performed))
+                }
+              })
+          })
+      })
+  }
 }
-        }
