c1ba243967f1e2c3e3a2bc6d2fbe95bcabadc7c3
beo_clowar <121174604+0xbugbag@users.noreply.github.com>
2024-12-04 22:42:28 +0700

    Update CorsHandler.scala

--- src/main/scala/com/example/CorsHandler.scala
+++ src/main/scala/com/example/CorsHandler.scala
@@ -7,23 +7,34 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Directive0, Route}
 
 trait CorsHandler {
-  private val corsResponseHeaders = List(
-    `Access-Control-Allow-Origin`.*,
+  // List of allowed origins - replace with your frontend URL
+  private val allowedOrigins = List(
+    "https://scala-backend.onrender.com",
+    "http://localhost:3000"  // for local development
+  )
+
+  private def corsResponseHeaders(origin: String) = List(
+    `Access-Control-Allow-Origin`(HttpOrigin(origin)),
     `Access-Control-Allow-Credentials`(true),
     `Access-Control-Allow-Headers`("Authorization", "Content-Type", "X-Requested-With"),
     `Access-Control-Allow-Methods`(OPTIONS, POST, PUT, GET, DELETE)
   )
 
-  // Wrap the Route with this to enable CORS
-  def corsHandler(r: Route): Route = addAccessControlHeaders {
-    preflightRequestHandler ~ r
+  def corsHandler(r: Route): Route = extractRequest { request =>
+    val origin = request.header[Origin].map(_.origins.head.toString)
+      .filter(allowedOrigins.contains)
+      .getOrElse(allowedOrigins.head)
+
+    addAccessControlHeaders(origin) {
+      preflightRequestHandler(origin) ~ r
+    }
   }
 
-  private def addAccessControlHeaders: Directive0 = {
-    respondWithHeaders(corsResponseHeaders)
+  private def addAccessControlHeaders(origin: String): Directive0 = {
+    respondWithHeaders(corsResponseHeaders(origin))
   }
 
-  private def preflightRequestHandler: Route = options {
-    complete(HttpResponse(StatusCodes.OK).withHeaders(corsResponseHeaders))
+  private def preflightRequestHandler(origin: String): Route = options {
+    complete(HttpResponse(StatusCodes.OK).withHeaders(corsResponseHeaders(origin)))
   }
 }
