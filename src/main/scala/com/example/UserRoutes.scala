b3f1f7ffe09dd7aca4579bd69493f79363af7a61
beo_clowar <121174604+0xbugbag@users.noreply.github.com>
2024-12-05 00:35:00 +0700

    Update UserRoutes.scala

--- src/main/scala/com/example/UserRoutes.scala
+++ src/main/scala/com/example/UserRoutes.scala
@@ -9,16 +9,10 @@ import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
-import spray.json.DefaultJsonProtocol._
-import spray.json.RootJsonFormat
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
 class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val system: ActorSystem[_]) extends CorsHandler {
-  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import JsonFormats._
-
-  // Add this for health check response
-  final case class HealthResponse(status: String, timestamp: String)
-  implicit val healthResponseFormat: RootJsonFormat[HealthResponse] = jsonFormat2(HealthResponse)
   
   private implicit val timeout: Timeout = 
     Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
